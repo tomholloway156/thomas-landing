bca7e6ac18518a85e841395f128a69b96f0f7dd1
cbravoquiroz <cbravo@talentsourcecorp.com>
2025-02-10 00:39:20 -0300

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,92 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Label } from "@radix-ui/react-label";
+import { Mail } from "lucide-react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const skills = [
+    "Data Analysis & Market Research",
+    "Music Consumer Behavior Insights",
+    "Strategic Marketing & Trend Forecasting",
+    "Audience Engagement & Social Listening",
+    "Competitive Analysis",
+    "Business Strategy",
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  function handleRedirectLinkedin(): void {
+    window.open("https://www.linkedin.com/in/tom-holloway-1993151bb/");
+  }
+
+  // function functionNewRamdom(): void {
+  //   console.log("Hola soy una nueva funcion");
+  // }
+
+  return (
+    <div className="min-h-screen bg-zinc-200 flex flex-col gap-4 items-center justify-center p-6 text-center">
+      {/* Sección una */}
+      <Card className="p-4 flex justify-center items-center flex-col">
+        <Label className="font-bold text-2xl text-gray-900 mb-4">
+          Consumer insights manager
+        </Label>
+        <img
+          className=" rounded-full w-52 object-contain"
+          src="./avatar.png"
+          alt=""
+        />
+        <p className="text-lg text-gray-700 max-w-2xl m-6">
+          Passionate about understanding music consumers through data analysis
+          and market research. Expertise in identifying trends, improving
+          marketing strategies, and enhancing customer experiences to drive
+          growth in the music industry.
+        </p>
+      </Card>
+
+      {/* Sección dos */}
+      <Card className="p-4 flex justify-center items-center flex-col">
+        <CardHeader className="text-2xl font-semibold text-gray-900 mb-4">
+          Skills & Expertise
+        </CardHeader>
+        <CardContent>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {skills.map((skill, index) => (
+              <Badge key={index} variant="default" className="p-1 text-center">
+                {skill}
+              </Badge>
+            ))}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button
+            variant={"link"}
+            // ir a una url externa
+            onClick={() => handleRedirectLinkedin()}
+            className="px-6 py-3 rounded-lg text-lg mt-6 hover:text-emerald-400"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Contact me
+            <Mail className="w-6 h-6 ml-2" />
+          </Button>
+
+{/* Botones para agregar nuevo */}
+          {/* <Button
+            variant={"link"}
+            // ir a una url externa
+            onClick={() => createfunction()}
+            className="px-6 py-3 rounded-lg text-lg mt-6 hover:text-emerald-400"
           >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            Contact me
+            <Mail className="w-6 h-6 ml-2" />
+          </Button> */}
+
+        </CardFooter>
+      </Card>
     </div>
   );
 }
